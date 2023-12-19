de7ceab3fb8099b4af23dc0c07a2fcdb81c29237
Nguyen Duc Tan <nguyenductan998@gmail.com>
2023-12-19 21:21:20 +0700

    nhúng bootstrap - và file tĩnh

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT;
 app.set("views", "./views");
 app.set("view engines", "pug");
 
+//bất kỳ tệp tĩnh nào (như hình ảnh, stylesheet, JavaScript, vv.) trong thư mục "public" sẽ có thể được truy cập qua đường dẫn URL của ứng dụng.
+// app.use(): là một middleware thực hiện các tác vụ trung gian trước khi xử lý yêu cầu từ người dùng, đến các tuyến đường (routes) khác.
 app.use(express.static("public"));
 
 
