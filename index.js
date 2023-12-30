7ec0368d6199caac85831dd7cfc9be3dc66f95ce
Nguyen Duc Tan <nguyenductan998@gmail.com>
2023-12-30 11:03:54 +0700

    add vercel.json file + fixed index.js file to deploy to vercal

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(methodOverride('_method'))
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.set("views", "./views");
+//⭐ app.set("views", `./views`);         // => Chỉ chạy được local
+app.set("views", `${__dirname}/views`);
 app.set("view engines", "pug");
 
 // FLASH
@@ -37,7 +38,9 @@ app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 // bất kỳ tệp tĩnh nào (như hình ảnh, stylesheet, JavaScript, vv.) trong thư mục "public" sẽ có thể được truy cập qua đường dẫn URL của ứng dụng.
 // app.use(): là một middleware thực hiện các tác vụ trung gian trước khi xử lý yêu cầu từ người dùng, đến các tuyến đường (routes) khác.
-app.use(express.static("public"));
+//⭐ console.log(__dirname)
+//⭐ app.use(express.static(`public`)); // => Chỉ chạy được local
+app.use(express.static(`${__dirname}/public`));
 
 
 // Routers
