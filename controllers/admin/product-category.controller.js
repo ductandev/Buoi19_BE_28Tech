e719d196ec9bf9e1407d684eeafeb378f6b37f19
Nguyen Duc Tan <nguyenductan998@gmail.com>
2024-01-11 22:34:46 +0700

    chinh sua danh muc san pham

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -4,86 +4,121 @@ const createTreeHelper = require("../../helpers/createTree.js")
 
 // [GET] /admin/products-category
 module.exports.index = async (req, res) => {
-    let find = {
-        deleted: false,
-    }
+  let find = {
+    deleted: false,
+  }
 
-    // // ĐỆ QUY (RECURSION TREE)
-    // function createTree(arr, parentId = "") {
-    //     const tree = [];
-    //     arr.forEach((item) => {
-    //         if (item.parent_id === parentId) {
-    //             // console.log("⭐⭐⭐⭐⭐ PASS")
-    //             const newItem = item;
-    //             const children = createTree(arr, item.id)       // khi bạn sử dụng "item.id" trong mã của bạn, Mongoose hiểu rằng bạn đang muốn truy cập trường _id và tự động chuyển đổi nó thành dạng chuỗi nếu cần thiết.
-    //             if (children.length > 0) {
-    //                 newItem.children = children;
-    //             }
-    //             tree.push(newItem);
-    //         }
-    //     });
-    //     return tree;
-    // }
+  // // ĐỆ QUY (RECURSION TREE)
+  // function createTree(arr, parentId = "") {
+  //     const tree = [];
+  //     arr.forEach((item) => {
+  //         if (item.parent_id === parentId) {
+  //             // console.log("⭐⭐⭐⭐⭐ PASS")
+  //             const newItem = item;
+  //             const children = createTree(arr, item.id)       // khi bạn sử dụng "item.id" trong mã của bạn, Mongoose hiểu rằng bạn đang muốn truy cập trường _id và tự động chuyển đổi nó thành dạng chuỗi nếu cần thiết.
+  //             if (children.length > 0) {
+  //                 newItem.children = children;
+  //             }
+  //             tree.push(newItem);
+  //         }
+  //     });
+  //     return tree;
+  // }
 
-    const records = await ProductCategory.find(find);
+  const records = await ProductCategory.find(find);
 
-    // const newRecords = createTree(records)           // Đệ quy chưa tách file
-    const newRecords = createTreeHelper.tree(records)
+  // const newRecords = createTree(records)           // Đệ quy chưa tách file
+  const newRecords = createTreeHelper.tree(records)
 
-    res.render("admin/pages/products-category/index.pug", {
-        pageTitle: "Danh mục sản phẩm",
-        records: newRecords
-    });
+  res.render("admin/pages/products-category/index.pug", {
+    pageTitle: "Danh mục sản phẩm",
+    records: newRecords
+  });
 }
 
 // [GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
-    let find = {
-        deleted: false
-    }
+  let find = {
+    deleted: false
+  }
 
-    // // ĐỆ QUY (RECURSION TREE)
-    // function createTree(arr, parentId = "") {
-    //     const tree = [];
-    //     arr.forEach((item) => {
-    //         // console.log("🚀 ~ file: product-category.controller.js:27 ~ arr.forEach ~ item:", item)
-    //         if (item.parent_id === parentId) {
-    //             // console.log("⭐⭐⭐⭐⭐ PASS")
-    //             const newItem = item;
-    //             const children = createTree(arr, item.id)       // khi bạn sử dụng "item.id" trong mã của bạn, Mongoose hiểu rằng bạn đang muốn truy cập trường _id và tự động chuyển đổi nó thành dạng chuỗi nếu cần thiết.
-    //             if (children.length > 0) {
-    //                 newItem.children = children;
-    //             }
-    //             tree.push(newItem);
-    //         }
-    //     });
-    //     return tree;
-    // }
+  // // ĐỆ QUY (RECURSION TREE)
+  // function createTree(arr, parentId = "") {
+  //     const tree = [];
+  //     arr.forEach((item) => {
+  //         // console.log("🚀 ~ file: product-category.controller.js:27 ~ arr.forEach ~ item:", item)
+  //         if (item.parent_id === parentId) {
+  //             // console.log("⭐⭐⭐⭐⭐ PASS")
+  //             const newItem = item;
+  //             const children = createTree(arr, item.id)       // khi bạn sử dụng "item.id" trong mã của bạn, Mongoose hiểu rằng bạn đang muốn truy cập trường _id và tự động chuyển đổi nó thành dạng chuỗi nếu cần thiết.
+  //             if (children.length > 0) {
+  //                 newItem.children = children;
+  //             }
+  //             tree.push(newItem);
+  //         }
+  //     });
+  //     return tree;
+  // }
 
-    const records = await ProductCategory.find(find)
+  const records = await ProductCategory.find(find)
 
-    // const newRecords = createTree(records)           // Đệ quy chưa tách file
-    const newRecords = createTreeHelper.tree(records)
+  // const newRecords = createTree(records)           // Đệ quy chưa tách file
+  const newRecords = createTreeHelper.tree(records)
 
-    res.render("admin/pages/products-category/create.pug", {
-        pageTitle: "Tạo danh mục sản phẩm",
-        records: newRecords
-    });
+  res.render("admin/pages/products-category/create.pug", {
+    pageTitle: "Tạo danh mục sản phẩm",
+    records: newRecords
+  });
 }
 
 // [POST] /admin/products-category/create
 module.exports.createPost = async (req, res) => {
-    if (req.body.position == "") {
-        const count = await ProductCategory.countDocuments();
-        req.body.position = +count + 1
-    } else {
-        req.body.position = parseInt(req.body.position)
-    }
+  if (req.body.position == "") {
+    const count = await ProductCategory.countDocuments();
+    req.body.position = +count + 1
+  } else {
+    req.body.position = parseInt(req.body.position)
+  }
 
-    // // if (req.file) {
-    // //   req.body.thumbnail = `/uploads/${req.file.filename}`
-    // // }
-    const records = await ProductCategory.create(req.body)
+  // // if (req.file) {
+  // //   req.body.thumbnail = `/uploads/${req.file.filename}`
+  // // }
+  const records = await ProductCategory.create(req.body)
 
-    res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+  res.redirect(`${systemConfig.prefixAdmin}/products-category`);
 };
+
+
+// [GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id
+
+    const data = await ProductCategory.findOne({
+      _id: id,
+      deleted: false
+    })
+
+    const records = await ProductCategory.find({
+      deleted: false
+    })
+
+    const newRecords = createTreeHelper.tree(records)
+
+    res.render("admin/pages/products-category/edit.pug", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/products-category/`)
+  }
+}
+
+// [PATCH] /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id
+
+  await ProductCategory.updateOne({ _id: id }, req.body)
+  res.redirect("back")
+}
