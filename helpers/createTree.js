f677802bce56a47726e9695d3dd769569b008977
Nguyen Duc Tan <nguyenductan998@gmail.com>
2024-01-11 11:39:08 +0700

    fix count

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,10 +1,13 @@
+let count = 0;
 const createTree = (arr, parentId = "") => {
     const tree = [];
     arr.forEach((item) => {
         // console.log("🚀 ~ file: product-category.controller.js:27 ~ arr.forEach ~ item:", item)
         if (item.parent_id === parentId) {
+            count++;
             // console.log("⭐⭐⭐⭐⭐ PASS")
             const newItem = item;
+            newItem.index = count;
             const children = createTree(arr, item.id)       // khi bạn sử dụng "item.id" trong mã của bạn, Mongoose hiểu rằng bạn đang muốn truy cập trường _id và tự động chuyển đổi nó thành dạng chuỗi nếu cần thiết.
             if (children.length > 0) {
                 newItem.children = children;
@@ -16,6 +19,7 @@ const createTree = (arr, parentId = "") => {
 }
 
 module.exports.tree = (arr, parentId = "") => {
+    count = 0;
     const tree = createTree(arr, parentId = "");
     return tree;
 }
