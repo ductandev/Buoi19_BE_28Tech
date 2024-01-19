bcb58f5815a470a86dbb47676a34c7b4393f5e21
Nguyen Duc Tan <nguyenductan998@gmail.com>
2024-01-19 23:44:29 +0700

    Tính năng gợi ý sản phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,34 @@
+const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/product.js")
 // [GET] /
 module.exports.index = async (req, res) => {
+    // =============Lấy ra sản phẩm nổi bật===================
+    const productsFeatured = await Product.find({
+        featured: "1",      // 1: nổi bật
+        deleted: false,
+        status: "active"
+    }).limit(6)
+    // =======================================================
+
+    // Hàm tính giá discount
+    const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
+
+    // =========== Hiển thị danh sách sản phẩm mới nhất ========
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({ position: "desc" }).limit(6);
+
+    const newProductsNew = productsHelper.priceNewProducts(productsNew);
+    // =========================================================
+
+
+
+
+
     res.render("client/pages/home/index.pug", {
-        pageTitle: "Trang chủ"
+        pageTitle: "Trang chủ",
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew
     });
 }
