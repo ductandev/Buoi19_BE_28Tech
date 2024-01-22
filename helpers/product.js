efaad80af4019324a3eee059d435640d5eab2913
Nguyen Duc Tan <nguyenductan998@gmail.com>
2024-01-22 13:38:48 +0700

    Trang chi tiết Client

--- helpers/product.js
+++ helpers/product.js
@@ -1,3 +1,4 @@
+// Tính giảm giá cho nhiều SP
 module.exports.priceNewProducts = (products) => {
     const newProducts = products.map(item => {
         item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0)
@@ -5,4 +6,11 @@ module.exports.priceNewProducts = (products) => {
     })
 
     return newProducts;
+}
+
+// Tính giảm giá cho 1 SP
+module.exports.priceNewProduct = (product) => {
+    const priceNew = (product.price * (100 - product.discountPercentage) / 100).toFixed(0)
+
+    return priceNew;
 }
