9f31aad246ee9610014827f81920691be3102c25
Nguyen Duc Tan <nguyenductan998@gmail.com>
2024-01-28 16:34:47 +0700

    + 2.3. Làm trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,50 +1,84 @@
-const Cart = require("../../models/cart.model.js")
+const Cart = require("../../models/cart.model.js");
+const Product = require("../../models/product.model.js");
+const productsHelper = require("../../helpers/product.js")
+
+// [GET] /cart
+module.exports.index = async (req, res) => {
+  const cartId = req.cookies.cartId;
+
+  const cart = await Cart.findOne({
+    _id: cartId
+  });
+
+  if (cart.products.length > 0) {
+    for (const item of cart.products) {
+      const productId = item.product_id
+
+      const productInfo = await Product.findOne({
+        _id: productId
+      })
+
+      productInfo.priceNew = productsHelper.priceNewProduct(productInfo)
+
+      item.productInfo = productInfo
+      item.totalPrice = item.quantity * productInfo.priceNew
+    }
+  }
+
+  cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0)
+
+  res.render("client/pages/cart/index.pug", {
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart
+  });
+}
+
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
-    const cartId = req.cookies.cartId
-    const productId = req.params.productId
-    const quantity = req.body.quantity
+  const cartId = req.cookies.cartId
+  const productId = req.params.productId
+  const quantity = req.body.quantity
 
 
 
-    const cart = await Cart.findOne({
-        _id: cartId
-    });
+  const cart = await Cart.findOne({
+    _id: cartId
+  });
 
-    const existProductInCart = cart.products.find(item => item.product_id == productId)
+  const existProductInCart = cart.products.find(item => item.product_id == productId)
 
-    if (existProductInCart) {
-        const newQuantity = +quantity + +existProductInCart.quantity
+  if (existProductInCart) {
+    const newQuantity = +quantity + +existProductInCart.quantity
 
-        await Cart.updateOne(
-            {
-                _id: cartId,
-                'products.product_id': productId
-            },
-            {
-                'products.$.quantity': newQuantity
-            }
-        );
+    await Cart.updateOne(
+      {
+        _id: cartId,
+        'products.product_id': productId
+      },
+      {
+        'products.$.quantity': newQuantity
+      }
+    );
 
-    } else {
-        const objectCart = {
-            product_id: productId,
-            quantity: quantity
-        }
-
-        await Cart.updateOne(
-            {
-                _id: cartId
-            },
-            {
-                $push: { products: objectCart }
-            }
-        )
+  } else {
+    const objectCart = {
+      product_id: productId,
+      quantity: quantity
     }
 
+    await Cart.updateOne(
+      {
+        _id: cartId
+      },
+      {
+        $push: { products: objectCart }
+      }
+    )
+  }
 
-    req.flash("success", "Thêm sản phẩm vào giỏ hàng thành công !")
 
-    res.redirect("back")
+  req.flash("success", "Thêm sản phẩm vào giỏ hàng thành công !")
+
+  res.redirect("back")
 }
--- routers/client/cart.route.js
+++ routers/client/cart.route.js
@@ -2,6 +2,10 @@ const express = require("express")
 const router = express.Router();
 
 const controller = require("../../controllers/client/cart.controller.js")
+
+
+
+router.get("/", controller.index)
 router.post("/add/:productId", controller.addPost)
 
 
