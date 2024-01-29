425719091c45e5aeae4d0dfa98582cddda7442a2
Nguyen Duc Tan <nguyenductan998@gmail.com>
2024-01-29 23:34:49 +0700

    fix bug vercel

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -1,36 +1,42 @@
-const Cart = require("../../models/cart.model.js")
+const Cart = require("../../models/cart.model.js");
 
 module.exports.cardId = async (req, res, next) => {
-    if (!req.cookies.cartId) {
-        const cart = new Cart;
-        await cart.save();
+    try {
+        if (!req.cookies.cartId) {
+            const cart = new Cart();
+            await cart.save();
 
-        const expiresTime = 1000 * 60 * 60 * 24 * 365
+            const expiresTime = 1000 * 60 * 60 * 24 * 365;
 
-        res.cookie("cartId", cart.id, {
-            expires: new Date(Date.now() + expiresTime)
-        })
-    } else {
-        const cart = await Cart.findOne({
-            _id: req.cookies.cartId
-        })
-
-        if (cart) {
-            // Kiểm tra xem cart không phải là null trước khi thực hiện các thao tác khác
-            cart.totalQuantity = cart.products ? cart.products.reduce((sum, item) => sum + item.quantity, 0) : 0;
+            res.cookie("cartId", cart.id, {
+                expires: new Date(Date.now() + expiresTime)
+            });
         } else {
-            // Xử lý khi cart là null
-            console.error("Error in cartId middleware: Cart is null");
-            res.status(500).send("Internal Server Error");
-            return;
+            const cart = await Cart.findOne({
+                _id: req.cookies.cartId
+            });
+
+            if (!cart) {
+                // Xử lý khi cart là null
+                console.error("Error in cartId middleware: Cart is null");
+                res.status(500).send("Internal Server Error");
+                return;
+            }
+
+            // Kiểm tra xem cart.products không phải là null trước khi thực hiện các thao tác khác
+            cart.totalQuantity = cart.products ? cart.products.reduce((sum, item) => sum + item.quantity, 0) : 0;
+
+            res.locals.miniCart = cart;
         }
 
-        res.locals.miniCart = cart;
-
+        next();
+    } catch (error) {
+        // Xử lý lỗi ở đây, ví dụ:
+        console.error("Error in cartId middleware:", error);
+        res.status(500).send("Internal Server Error");
     }
+};
 
-    next();
-}
 
 
 
